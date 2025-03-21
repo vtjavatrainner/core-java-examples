847700421107b3868036ab6e69f0cd224e1a58e5
Ashwini Tadge <ashwinitaware17@gmail.com>
2025-03-21 08:56:21 +0530

    List test file added

---
+++ src/com/pcstraining/core/collection/ListTest.java
@@ -0,0 +1,14 @@
+package com.pcstraining.core.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Mango");
+        list.add("Grapes");
+        System.out.println(list);
+    }
+}
