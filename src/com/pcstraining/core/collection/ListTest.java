d50950a274838d763baadec5c6242245398c083c
vtjavatrainner <vtpcsjavatrainner@gmail.com>
2025-03-21 09:04:30 +0530

    Changed variable name

--- src/com/pcstraining/core/collection/ListTest.java
+++ src/com/pcstraining/core/collection/ListTest.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListTest {
-
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("Mango");
-        list.add("Grapes");
-        System.out.println(list);
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Mango");
+        fruits.add("Grapes");
+        System.out.println(fruits);
     }
 }
